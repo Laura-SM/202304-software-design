c59c15dacec8a987c23bab64ec7f116ebb91e9c8
Laura-SM <74296613+Laura-SM@users.noreply.github.com>
2023-06-01 17:48:19 +0200

    Add comment

--- src/TicketDispenser/TicketDispenser/TicketDispenser.test.ts
+++ src/TicketDispenser/TicketDispenser/TicketDispenser.test.ts
@@ -1,9 +1,10 @@
 import TicketDispenser from './TicketDispenser';
 import TurnNumberSequence from '../TurnNumberSequence/TurnNumberSequence';
+import { Sequence } from '../types';
 
 describe('Given a Turn Ticket Dispenser', () => {
-  let turnNumberSequence: TurnNumberSequence;
-
+  let turnNumberSequence: Sequence<number>;
+  // Instead of using the implementation of the class TurnNumberSequence, it is better to create a mock class. Doing this, if class TurnNumberSequence fails, the tests will continue to pass.
   beforeEach(() => {
     turnNumberSequence = new TurnNumberSequence();
   });
